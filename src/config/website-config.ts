944080c106a502f2fd241850b9e62dad5d4d85a0
Michel Paquette <paquettm@vaniercollege.qc.ca>
2025-09-07 21:39:26 -0400

    URL fix

--- src/config/website-config.ts
+++ src/config/website-config.ts
@@ -5,12 +5,12 @@ export default {
   title: '420-302-VA Internet of Things - Introduction to Python Programming',
 
   // The URI of the site. This is used in the <head> of the page.
-  url: 'https://paquettm.github.io/420-302-VA_A25_private/',
+  url: 'https://paquettm.github.io/420-302-VA_A25/',
 
   // The base directory of the site. Don't include a trailing slash.
-  baseDirectory: '/420-302-VA_A25_private', // TODO: set the base path for the site if you are deploying to a subdirectory (recommended) on GitHub Pages. The directory name must match the name of the GitHub repo.
+  baseDirectory: '/420-302-VA_A25', // TODO: set the base path for the site if you are deploying to a subdirectory (recommended) on GitHub Pages. The directory name must match the name of the GitHub repo.
 
-  gitHubRepoUri: 'https://github.com/paquettm/420-302-VA_A25_private',
+  gitHubRepoUri: 'https://github.com/paquettm/420-302-VA_A25',
 
   // The path to the favicon
   // You can use: https://www.svgrepo.com/
